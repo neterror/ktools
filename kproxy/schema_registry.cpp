769f9afb5789350de6a12f90b8dc72f29541d861
Plamen Todorov <plamen@abrites.com>
2025-06-13 09:36:00 +0300

    url encode the registry subject name
    the subject names may contain slashes

--- kproxy/schema_registry.cpp
+++ kproxy/schema_registry.cpp
@@ -131,8 +131,11 @@ void SchemaRegistry::deleteSchema(const QString& subject, qint32 version) {
 
 
 void SchemaRegistry::deleteSchema(const QString& subject, bool permanently) {
-    QString url = QString("subjects/%1").arg(subject);
-    mRest.deleteResource(requestV3(url), this, [this,subject,permanently](QRestReply &reply) {
+    QString encodedSubject = QUrl::toPercentEncoding(subject, QByteArray(), "/");
+    QString url = QString("subjects/%1").arg(encodedSubject);
+    //1. Do a soft delete (as described here): https://docs.confluent.io/platform/current/schema-registry/schema-deletion-guidelines.html#hard-delete-schema
+    qDebug().noquote() << "soft delete:" << url;
+    mRest.deleteResource(requestV3(url), this, [this,encodedSubject,permanently](QRestReply &reply) {
         auto json = reply.readJson();
         if (json && json->isObject()) {
             auto obj = json->object();
@@ -147,9 +150,12 @@ void SchemaRegistry::deleteSchema(const QString& subject, bool permanently) {
             return;
         }
             
+	//2. Now do the hard delete
         //repeat again the deletion, this time with permanent=true
-        QString url = QString("subjects/%1?permanent=true").arg(subject);
-        mRest.deleteResource(requestV3(url), this, [this,subject,permanently](QRestReply &reply) {
+        QString url = QString("subjects/%1?permanent=true").arg(encodedSubject);
+	qDebug().noquote() << "permanent delete:" << url;
+
+        mRest.deleteResource(requestV3(url), this, [this,encodedSubject,permanently](QRestReply &reply) {
             auto json = reply.readJson();
             if (json && json->isObject()) {
                 auto obj = json->object();
